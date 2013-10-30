ba857f1017c34eebce46145efabee8dc8ef3a55c
Sean Heintz <sean.heintz@gmail.com>
2013-10-29 21:11:17 -0600

    Started on client manager.

---
+++ KLFServer/ClientManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+
+namespace KMPServer
+{
+    class ClientManager
+    {
+        private List<ServerClient> m_clientList;
+        private Server m_server;
+
+        public ClientManager(int max_size, ref Server parent_server)
+        {
+            this.m_clientList = new List<ServerClient>(max_size);
+            this.m_server = parent_server;
+        }
+
+        public bool addClient(TcpClient client)
+        {
+            ServerClient user = new ServerClient(this.m_server, m_clientList.Count);
+
+            return true;
+        }
+    }
+}
